528ec6d20c2fcfba18f8b1d13c036dbe397b9ee1
sarafina-chea <123130516+sarafina-chea@users.noreply.github.com>
2025-02-14 10:00:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, my sweetie...`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, <3...`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
