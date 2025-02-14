157cc27c8f6adbacc49b8610e61e225e190a3605
sarafina-chea <123130516+sarafina-chea@users.noreply.github.com>
2025-02-13 22:23:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "IS THAT EVEN A QUESTION??? ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you like me?",                          // For the love meter
